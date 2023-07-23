33ce9ae73283e88226213555d929842626ea89f3
bunhor-deb <bunhor0303@gmail.com>
2023-07-23 23:31:03 +0700

    done assignment 01

---
+++ 01-Fundamentals-Part-1/starter/assigment/01-value-and-variable/script.js
@@ -0,0 +1,10 @@
+let country, continent, population;
+
+country = 'Cambodia';
+continent = 'Asia';
+population = 16590000;
+
+console.log('Country name is ' 
++ country + ', Its location at ' 
++ continent + ' continent and have population is ' 
++ population)
