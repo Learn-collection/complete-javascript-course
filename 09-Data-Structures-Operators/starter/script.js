e0fbc65b868c87dd8be24acf251e2e0176c4f1d2
bunhor-deb <bunhor0303@gmail.com>
2023-08-20 07:47:33 +0700

    Learn function

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -71,14 +71,14 @@
 
 
 
-const rest1 = {
-  name: 'J-kop',
-  numGuests: 0,
-};
-const rest2 = {
-  name: 'Kakiya',
-  owner: 'Le dav',
-};
+// const rest1 = {
+//   name: 'J-kop',
+//   numGuests: 0,
+// };
+// const rest2 = {
+//   name: 'Kakiya',
+//   owner: 'Le dav',
+// };
 
 
 //-- OR assigment operator
@@ -90,29 +90,138 @@ const rest2 = {
 // rest2.numGuests ||= 10;
 
 //nullish assigment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-console.log(rest1);
-console.log(rest2);
-
-
+// console.log(rest1);
+// console.log(rest2);
 
 
 
 
+// const menu = [...restaurant.starterMenu, ...restaurant
+// .mainMenu];
+
+// for(const item of menu) console.log(item);
+// for(const [i, el] of menu.entries()){
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 
 
+// -- Challeng #2 --
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
 
+// Sovle the challeng #2
+// for (const [goalNum, scorer] of game.scored.entries()){
+//   console.log(`Goal ${goalNum}: ${scorer}`);
+// };
+
+// const calAvg = function (){
+//   const result = Object.values(game.odds);
+//   let avg = 0;
+//   for (const v of result) avg += v;
+//   avg = avg/result.length;
+//   console.log(avg)
+// };
+// calAvg();
+
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
+
+//-- Map -- 
+// const question = new Map([
+//   ['question', 'what is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'Javascript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try again!'],
+// ]);
+// console.log(question);
+
+// //Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of  question){
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));3
 
 
+// -- Challenge #3
+
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
+
+// const newEvent = [...new Set(gameEvents.values())];
+
+// console.log(newEvent);
 
 
+//-- Work with string
 
+const airline = 'Tap Air Portugal';
+const plane = 'A320';
 
-
+console.log(plane[0])
 
 
 
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,29 @@
-'use strict';
+// 'use strict';
+// let f;
+
+// const g = function () {
+//     const a = 23;
+//     f = function () {
+//         console.log(a * 2);
+//     };
+// };
+
+// const h = function () {
+//     const b = 777;
+//     f = function () {
+//         console.log(b * 2);
+//     };
+
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// //Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// -- Coding Challeng
+
