660434ddf70fefddf93cb4fce0f2a6eb9df1f811
deb.bunhor <deb.bunhor@sathapana.com.kh>
2023-11-25 20:28:10 +0700

    learning array

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -73,71 +73,95 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log("-- For of --");
+console.log('-- For of --');
 
-for (const [i, movement] of movements.entries()){
-  if(movement > 0){
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
     console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  };
-};
+  }
+}
 
-console.log("-- For Each --");
+console.log('-- For Each --');
 
-movements.forEach(function(movement, index, array){
-  if(movement > 0) {
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
     console.log(`You deposited ${movement}`);
-  }else{
+  } else {
     console.log(`You withdrew ${Math.abs(movement)}`);
   }
-})
-
+});
 
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
 
   movements.forEach(function (movement, i) {
-
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
         <div class="movements__value">${movement}</div>
       </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
-}
+};
 displayMovements(account1.movements);
 
-const user = 'Steven Thomas Williams'
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, move) => acc + move, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}`;
 
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}`;
+};
+calcDisplaySummary(account1.movements);
+
+const user = 'Steven Thomas Williams';
 
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
-    .toLowerCase()
-    .split(' ')
-    .map(name => name[0])
-    .join('');
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
   });
 };
 
 createUsername(accounts);
 
-console.log(accounts);
-
 /////////////////////////////////////////////////
 
-console.log(movements);
+// console.log(movements);
 
-const deposits = movements.filter(mov => mov > 0);
-console.log(deposits);
-const widthdraws = movements.filter(mov => mov < 0);
-console.log(widthdraws);
-
+// const deposits = movements.filter(mov => mov > 0);
+// console.log(deposits);
+// const widthdraws = movements.filter(mov => mov < 0);
+// console.log(widthdraws);
 
 ////////////////////////////////////////////////
 
@@ -159,26 +183,46 @@ console.log(widthdraws);
 // console.log(movementsUSDfor);
 
 // const movementDesc = movements.map(
-//   (mov, i) => 
-//   `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'widthdraw'} ${Math.abs(mov)}` 
+//   (mov, i) =>
+//   `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'widthdraw'} ${Math.abs(mov)}`
 // )
 
 // console.log(movementDesc);
 
 //// End learn a bout Map
+//// Reduce method ////
 
+//Accumulator is like a snowball
+// console.log(movements);
+// const balance = movements.reduce((acc, cur) => acc + cur, 0);
+// console.log(balance);
+/////////////////////////////////
 
+// Coding Challenge #1
 
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
 
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
 
+HINT: Use tools from all lectures in this section so far 😉
 
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
+GOOD LUCK 😀
+*/
 
+// const juliaDatas = [3, 5, 2, 12, 7];
+// const kateData = [4, 1, 15, 8, 3];
 
+// const juliaDoges = juliaDatas.slice(1, 3);
 
+// console.log(juliaDoges);
 
-
-
-
-
+// const checkDogs = function (dogejilia, dogsKate) {};
