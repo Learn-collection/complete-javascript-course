d7c828996f8815bb153a57176887ed4ef3837605
deb.bunhor <deb.bunhor@sathapana.com.kh>
2024-02-29 16:02:39 +0700

    learning

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,6 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 === 23.0); 
+console.log(0.1+0.2);
+console.log(0.1 + 0.2 === 0.3);
