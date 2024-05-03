89244fe07cc71739eb5a56d1d2ee2f28e3c0d687
deb.bunhor <deb.bunhor@sathapana.com.kh>
2024-05-03 19:56:27 +0700

    learn javascript

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,64 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+/////////////////////////////
+///////////////////////////
+////////////////////////////
+// Selecting Elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+//  const allSeactions = document.querySelectorAll('.section');
+//  console.log(allSeactions);
+
+//  const allButtons = document.getElementsByTagName('button');
+//  console.log(allButtons);
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We user cookies for improved functionality and annalytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+header.before(message);
+
+//Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+// Style
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 40 + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  // Scrolling
+  const s1coords = section1.getBoundingClientRect();
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  window.scrollTo({
+    left: s1coords.left + window.scrollX,
+    top: s1coords.top + window.scrollY,
+    behavior: 'smooth',
+  });
+});
+
